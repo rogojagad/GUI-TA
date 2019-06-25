3062b97179b6b997a1b1d13579fb68ec5f16ee70
rogojagad <rogojagadalit@gmail.com>
2019-06-25 20:50:58 +0700

    more polite way to do task on server booting up

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 from process_controller import ProcessController
+from pprint import pprint
 import json
 
 app = Flask(__name__)
-controller = ProcessController()
+controller = object()
 
 
 @app.route("/", methods=["GET"])
@@ -13,10 +14,18 @@ def index():
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    sentence = request.get_json()["sentence"]
+    sentence = request.get_json(silent=True)["sentence"]
+    pprint(sentence)
+    # sentence = request.get_json(force=True)["sentence"]
 
     return json.dumps(controller.start_process(sentence))
-    # return controller.start_process()
+    # return request.form.get("sentence")
+
+
+@app.before_first_request
+def initialize():
+    global controller
+    controller = ProcessController()
 
 
 if __name__ == "__main__":
